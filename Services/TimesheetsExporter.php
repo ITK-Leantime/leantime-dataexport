3e5b6eae5f57cfa0c7e15259def6c2c853276f88
martinyde <yde001@gmail.com>
2024-03-22 10:28:46 +0100

    Applied coding standards

--- Services/TimesheetsExporter.php
+++ Services/TimesheetsExporter.php
@@ -28,20 +28,20 @@ final class TimesheetsExporter extends AbstractExporter
      */
     public function generateData(array $criteria): array
     {
-        $projectId = (int) ($criteria[ 'project' ] ?? 0);
-        $kind = $criteria[ 'kind' ] ?? null;
-        $dateFrom = $this->getDateTime($criteria[ 'dateFrom' ] ?? null, new Carbon('2000-01-01'));
-        $dateTo = $this->getDateTime($criteria[ 'dateTo' ] ?? null, new Carbon('2100-01-01'));
-        $userId = (int) ($criteria[ 'userId' ] ?? 0);
-        $invEmpl = filter_var($criteria[ 'invEmpl' ] ?? null, FILTER_VALIDATE_BOOLEAN) ? '1' : '';
-        $invComp = filter_var($criteria[ 'invComp' ] ?? null, FILTER_VALIDATE_BOOLEAN) ? '1' : '';
-        $paid = filter_var($criteria[ 'paid' ] ?? null, FILTER_VALIDATE_BOOLEAN) ? '1' : '';
-        $clientId = (int) ($criteria[ 'clientId' ] ?? 0);
+        $projectId = (int) ($criteria['project'] ?? 0);
+        $kind = $criteria['kind'] ?? null;
+        $dateFrom = $this->getDateTime($criteria['dateFrom'] ?? null, new Carbon('2000-01-01'));
+        $dateTo = $this->getDateTime($criteria['dateTo'] ?? null, new Carbon('2100-01-01'));
+        $userId = (int) ($criteria['userId'] ?? 0);
+        $invEmpl = filter_var($criteria['invEmpl'] ?? null, FILTER_VALIDATE_BOOLEAN) ? '1' : '';
+        $invComp = filter_var($criteria['invComp'] ?? null, FILTER_VALIDATE_BOOLEAN) ? '1' : '';
+        $paid = filter_var($criteria['paid'] ?? null, FILTER_VALIDATE_BOOLEAN) ? '1' : '';
+        $clientId = (int) ($criteria['clientId'] ?? 0);
 
         $data = $this->timesheets->getAll($projectId, $kind, $dateFrom, $dateTo, $userId, $invEmpl, $invComp, $paid, $clientId, 0);
 
-        foreach ($data as &$row) {           
-            $row['fullname'] =  $row['firstname'].' '.$row['lastname'];
+        foreach ($data as &$row) {
+            $row['fullname'] =  $row['firstname'] . ' ' . $row['lastname'];
         }
 
         return $data;
