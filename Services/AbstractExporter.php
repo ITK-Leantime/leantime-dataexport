371e8de85034d0c89f486ba71d45eee8cfcec6b2
Sine Jespersen <sinejespersen@gmail.com>
2024-09-03 12:53:42 +0200

    2144: desperately add phpstan-ignore-line

--- Services/AbstractExporter.php
+++ Services/AbstractExporter.php
@@ -75,7 +75,7 @@ abstract class AbstractExporter
     {
         $value ??= $default;
 
-        return null !== $value ? $this->dateTimeHelper->parseUserDateTime($value)->setToDbTimezone() : null;
+        return null !== $value ? $this->dateTimeHelper->parseUserDateTime($value)->setToDbTimezone() : null; // @phpstan-ignore-line
     }
 
     /**
